6ec43012d15e721e3e1296f75d86923d5310fe3e
Shingfling <100726546+Shingfling@users.noreply.github.com>
2023-03-28 21:59:12 -0400

    change order of patterns
    this is so pattern 1 is the short pattern for selection purposes.

--- crochet2.0/src/testPatterns.js
+++ crochet2.0/src/testPatterns.js
@@ -1,4 +1,15 @@
 export const testPatterns = {
+    patternShort: [
+        [{name: "SC", value: 1, complete: false},{name: "SC", value: 1, complete: false},{name: "SC", value: 1, complete: false},],
+        [{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false}, {name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},],
+        [{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},],
+        [{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false}, {name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},],
+        [{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false}, {name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false}, {name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},],
+        [{name: "SC", value: 1, complete: false},{name: "SC", value: 1, complete: false},{name: "SC", value: 1, complete: false},],
+        [{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false}, {name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},],
+        [{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},],
+        [{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false}, {name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},],
+        [{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false}, {name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false}, {name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},],],
     patternLong: [
         [{name: "SC", value: 1, complete: false},{name: "SC", value: 1, complete: false},{name: "SC", value: 1, complete: false},],
         [{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false}, {name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},],
@@ -71,16 +82,5 @@ export const testPatterns = {
         [{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false}, {name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},],
         [{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false}, {name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false}, {name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},]
         
-      ],
-    patternShort: [
-        [{name: "SC", value: 1, complete: false},{name: "SC", value: 1, complete: false},{name: "SC", value: 1, complete: false},],
-        [{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false}, {name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},],
-        [{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},],
-        [{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false}, {name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},],
-        [{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false}, {name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false}, {name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},],
-        [{name: "SC", value: 1, complete: false},{name: "SC", value: 1, complete: false},{name: "SC", value: 1, complete: false},],
-        [{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false}, {name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},],
-        [{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},],
-        [{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false}, {name: "SC", value: 0, complete: false},{name: "INC", value: 1, complete: false},{name: "SC", value: 0, complete: false},],
-        [{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false}, {name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false}, {name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},{name: "SC", value: 0, complete: false},],]
+      ]
   };
